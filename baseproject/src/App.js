45bdd9b9b2d92e489b92e4ea16d215039be1fc57
Brahmananda <brahmananda203@gmail.com>
2022-03-03 21:23:02 +0530

    React-App

---
+++ baseproject/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>this is main</main>
+    </div>
+  );
+}
+
+export default App;
